988d7a74d98cb9d87c9a5bba5bc6cb64c3340272
sdurighello <stefano.durighello@gmail.com>
2017-07-02 16:59:04 +0200

    removed Counter route

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -5,10 +5,8 @@ import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
+    <h1>GitHub API Assignment</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
     {' · '}
     <Link to='/issues' activeClassName='page-layout__nav-item--active'>Issues</Link>
     <div className='page-layout__viewport'>
